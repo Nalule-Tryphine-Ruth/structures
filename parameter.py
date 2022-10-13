0a6217c3d2622a690f88dc568656c4a73be3899e
Nalule-Tryphine-Ruth <113919554+Nalule-Tryphine-Ruth@users.noreply.github.com>
2022-10-13 16:08:38 +0300

    Add files via upload

---
+++ parameter.py
@@ -0,0 +1,18 @@
+
+from unicodedata import name
+
+
+def user_prompt(name):
+    
+    
+   print("What is your name?")
+   
+   print("Your name is:",name)
+
+#    print ("What is your age?")
+#    age=int(input())
+
+#    print("Your age is :",age)
+    
+user_prompt("Crystal")
+user_prompt("Tryphine")
