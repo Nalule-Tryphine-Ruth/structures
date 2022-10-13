d243ace177d26cd02ba4e60fb5a46fd82f3fc153
Nalule-Tryphine-Ruth <113919554+Nalule-Tryphine-Ruth@users.noreply.github.com>
2022-10-13 16:08:09 +0300

    Add files via upload

---
+++ user_prompt.py
@@ -0,0 +1,19 @@
+
+
+
+
+def user_prompt():
+    
+    
+   print("What is your name?")
+   name=input()
+
+   print("Your name is:",name)
+
+   print ("What is your age?")
+   age=int(input())
+
+   print("Your age is :",age)
+    
+user_prompt()
+user_prompt()
