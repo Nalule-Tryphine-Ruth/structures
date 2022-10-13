1f106b291499ec4ffa660bfb564d212efcfa488f
Nalule-Tryphine-Ruth <113919554+Nalule-Tryphine-Ruth@users.noreply.github.com>
2022-10-13 16:07:46 +0300

    Add files via upload

---
+++ functiontutorial.py
@@ -0,0 +1,10 @@
+print("What is your name?")
+
+name= input("Please enter your name:")
+print(name)
+if name=="":
+    print("Please enter name")
+elif name ==name:
+    print(f"Hello,{name}")
+else:
+    print("Goodbye")
