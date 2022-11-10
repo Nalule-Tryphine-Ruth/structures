06157fbc182bd2acbe3b11183f0753d65a0c29cb
Nalule-Tryphine-Ruth <113919554+Nalule-Tryphine-Ruth@users.noreply.github.com>
2022-11-10 21:44:35 +0300

    Update user_prompt.py

--- user_prompt.py
+++ user_prompt.py
@@ -17,3 +17,26 @@ def user_prompt():
     
 user_prompt()
 user_prompt()
+
+
+
+
+
+
+
+
+def user_prompt():
+    
+    
+   print("What is your name?")
+   
+
+   print("Your name is:",name)
+
+   print ("What is your age?")
+   age=int(input())
+
+   print("Your age is :",age)
+    
+user_prompt()
+user_prompt()
